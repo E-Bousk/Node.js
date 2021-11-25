bb95844d36e4b4031ee715106f6f331a2aa8c483
E.Bousk <e.bousk@gmail.com>
2021-11-25 12:48:36 +0100

    Routing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// On appelle le module FS (filesystem) que l'on met dans une variable
-// On obtient ainsi un objet qui contient un tas de fonctionalités pour lire/écrire des fichiers
-const fs = require('fs');
+// // ************************************
+// // *****   Gestion de fichiers    *****
+// // ************************************
+// // On appelle le module FS (filesystem) que l'on met dans une variable
+// // On obtient ainsi un objet qui contient un tas de fonctionalités pour lire/écrire des fichiers
+// const fs = require('fs');
 
-// *******************************
-// ** Blocking, synchronous way **
-// *******************************
-
+// // ////////////////////////////////////
+// // //   Blocking, synchronous way    //
+// // ////////////////////////////////////
 // // 1er argument : le chemin du fichier, 2eme argument l'encodage des caractères (si rien de precisé, on obtient un 'buffer')
 // // Appeller cette fonction va lire le fichier et le mettre dans une variable 'textIn'
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -19,10 +21,9 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File created!');
 
-
-// ************************************
-// ** Non-blocking, asynchronous way **
-// ************************************
+// // ////////////////////////////////////
+// // // Non-blocking, asynchronous way //
+// // ////////////////////////////////////
 // // 1er argument : le chemin du fichier, 2eme argument l'encodage des caractères, 3eme argument : une 'callback function'
 // // *** Comportement asynchrone : NODEJS commence à lire le fichier en background,
 // // et dès qu'il sera prêt, il démarrera la fonction (callback) spécifiée en 2nd argument...
@@ -52,22 +53,51 @@ const fs = require('fs');
 // console.log('Will read file!');
 
 
+// // ************************************
+// // *****  Creation d'un serveur   *****
+// // ************************************
+// //On inclus un autre module/package (appellé HTTP)
+// const http = require('http');
+
+// // On utilise la méthode "createServer" qui est dans l'objet "http"
+// // À chaques requêtes sur le serveur, une 'callback function' sera appelée
+// // cette 'callback function' a deux arguments : 1er REQuest et 2nd RESponse
+// const server = http.createServer((req, res) => {
+//   res.end('Hello 👋 from the server!');
+// });
+
+// // Méthode "listen" démarre le serveur et écoute les connections/requêtes
+// // 1er paramètre : le port, 2ème : le host, 3eme optionel : une 'callback function' 
+// // (qui sera exécutée dès que le serveur aura commencé à écouter)
+// server.listen(8000, '127.0.0.1', () => {
+//   console.log('Listening to requests on port 8000');
+// });
+
 // ************************************
-// *****  Creation d'un serveur   *****
+// *****   Creation d'un router   *****
 // ************************************
-//On inclus un autre module/package (appellé HTTP)
 const http = require('http');
 
-// On utilise la méthode "createServer" qui est dans l'objet "http"
-// À chaques requêtes sur le serveur, une 'callback function' sera appelée
-// cette 'callback function' a deux arguments : 1er REQuest et 2nd RESponse
+// On utilise le build-in node module "url"
+const url = require('url');
+
 const server = http.createServer((req, res) => {
-  res.end('Hello 👋 from the server!');
+  console.log('req.url => ', req.url);
+
+  const pathName = req.url;
+  if(pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
-// Méthode "listen" démarre le serveur et écoute les connections/requêtes
-// 1er paramètre : le port, 2ème : le host, 3eme optionel : une 'callback function' 
-// (qui sera exécutée dès que le serveur aura commencé à écouter)
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
